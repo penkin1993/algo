dfd86855a7cd1ae642fc6a43df868ad0f5fb0475
Григорий Пенкин <penkin.g@yandex.ru>
2019-11-23 16:58:15 +0800

    9

---
+++ ex_9/cmake-build-debug/CMakeFiles/ex_9.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex_9.dir/main.cpp.o"
+  "ex_9"
+  "ex_9.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex_9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
