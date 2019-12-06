08bf9f8b487041937d326f44d87a99208ee624ed
Григорий Пенкин <penkin.g@yandex.ru>
2019-12-07 01:32:37 +0300

    9.1testpass

---
+++ ex__9/cmake-build-debug/CMakeFiles/ex__9.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ex__9.dir/main.cpp.o"
+  "ex__9"
+  "ex__9.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ex__9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
